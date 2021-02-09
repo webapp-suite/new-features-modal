3ba9767ea29360bee145b67655401adad8bc02c9
Kimi Gao <kimi.gao@outlook.com>
2021-02-09 21:50:21 +0800

    add skeleton for image

--- src/NewFeatureModal/index.tsx
+++ src/NewFeatureModal/index.tsx
@@ -1,10 +1,12 @@
-import "@tradeshift/elements.root";
-import "@tradeshift/elements.button";
-import "@tradeshift/elements.modal";
-import "./index.css";
+import '@tradeshift/elements.root';
+import '@tradeshift/elements.button';
+import '@tradeshift/elements.modal';
+import './index.css';
 
-import React, { useState } from "react";
-import { adapt } from "webcomponents-in-react";
+import React, { Suspense, useState } from 'react';
+import ContentLoader from 'react-content-loader';
+import { useImage } from 'react-image';
+import { adapt } from 'webcomponents-in-react';
 
 const KEY_PREFIX = "new-features-modal";
 
@@ -17,6 +19,19 @@ const Modal = adapt("ts-modal", {
     onOpen: "open",
     noPadding: "no-padding",
 });
+
+interface ImageProps {
+    src: string;
+    alt: string;
+}
+
+const Image = (props: ImageProps) => {
+    const { src } = useImage({
+        srcList: props.src,
+    });
+
+    return <img src={src} alt={props.alt} />;
+};
 
 interface Feature {
     title: string;
@@ -99,10 +114,31 @@ const NewFeaturesModal: React.FC<NewFeaturesModalProps> = ({
         >
             <div slot="main">
                 <div className="img-container">
-                    <img
-                        src={validFeatures?.[featCurIdx]?.img}
-                        alt={validFeatures?.[featCurIdx]?.title}
-                    />
+                    <Suspense
+                        fallback={
+                            <ContentLoader
+                                viewBox="0 0 100% 200"
+                                height={250}
+                                width="100%"
+                                backgroundColor="#f3f3f3"
+                                foregroundColor="#fff"
+                            >
+                                <rect
+                                    x="0"
+                                    y="0"
+                                    rx="5"
+                                    ry="5"
+                                    width="100%"
+                                    height="100%"
+                                />
+                            </ContentLoader>
+                        }
+                    >
+                        <Image
+                            src={validFeatures?.[featCurIdx]?.img}
+                            alt={validFeatures?.[featCurIdx]?.title}
+                        />
+                    </Suspense>
                 </div>
                 <h1 className="title">{validFeatures?.[featCurIdx]?.title}</h1>
                 <div className="description">
